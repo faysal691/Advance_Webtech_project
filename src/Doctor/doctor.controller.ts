1f495e3115427821a94d1622d2b8e9be05895630
sharearahammed <sharearahammed@gmail.com>
2023-10-02 22:14:55 +0600

    commit

--- src/Doctor/doctor.controller.ts
+++ src/Doctor/doctor.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query ,Delete,Body, Put,Post } from '@nestjs/common';
+import { Controller, Get, Param, Query ,Delete,Body, Put,Post, Patch } from '@nestjs/common';
 import { DoctorService } from './doctor.service';
 import { Doctorinfo,CreateDoctorDto,UpdateDoctorDto } from './doctorrinfo.dto';
 
@@ -14,17 +14,14 @@ export class DoctorController {
   getIndex(): string {
     return "hello index";
   } 
-
   @Get('/searchuserby/:id')
   getUser(@Param('id') id:number): string {
     return "the user is"+id;
   }
-
   @Get('/searchuserby/:name')
   getName(@Param('id') name:string): string {
     return "The name is"+name;
   }
-
   @Get('/getuserby')
   getUserByNameAndId(@Query('name') name: string, @Query('id') id:number): string {
     return "the name is "+name+" and ID id "+id;
@@ -33,27 +30,47 @@ export class DoctorController {
   getUserByBody(@Body('name') name: string, @Body('id') id:number): object{
     return {name,id};
   }
-
   @Post('/adduserobject')
   addUserByNameAndId(@Body('name') name: string, @Body('id') id:number): string {
     return "the name is "+name+" and ID id "+id;
   }
-
   @Post('/adduser')
   addUserByObject(@Body() user:Doctorinfo ): object {
     return {user};
   }
-
-
-
   @Post('/create')
   create(@Body()createDoctorDto:CreateDoctorDto):any{
     return {message : 'Doctor Created', data:CreateDoctorDto};
   }
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateDoctorDto: UpdateDoctorDto): any {
+    return { message: `Doctor with ID ${id} updated`, data: updateDoctorDto };
+  }
+  @Patch(':id')
+  partialUpdate(@Param('id') id: string, @Body() updateDoctorDto: UpdateDoctorDto): any {
+    return { message: `Doctor with ID ${id} partially updated`, data: updateDoctorDto };
+  }
+  @Delete(':id')
+  remove(@Param('id') id: string): any {
+    return { message: `Doctor with ID ${id} deleted` };
+  }
+  @Get(':id/patients')
+  findPatients(@Param('id') id: string): any {
+    return { message: `Patients of Doctor with ID ${id}` };
+  }
 
-  @Post('/update')
-  update(@Body()updateDoctorDto:UpdateDoctorDto):any{
-    return {message : 'Doctor Updated', data:UpdateDoctorDto};
+  @Post(':id/patients')
+  addPatient(@Param('id') id: string, @Body() patientData: any): any {
+    return { message: `Patient added to Doctor with ID ${id}`, data: patientData };
+  }
+  @Get(':id/appointments')
+  findAppointments(@Param('id') id: string): any {
+    return { message: `Appointments of Doctor with ID ${id}` };
+  }
+
+  @Post(':id/appointments')
+  createAppointment(@Param('id') id: string, @Body() appointmentData: any): any {
+    return { message: `Appointment created for Doctor with ID ${id}`, data: appointmentData };
   }
 
 }
