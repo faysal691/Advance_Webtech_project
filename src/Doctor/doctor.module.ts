c42778075b665b09d96477e0b5656ab9631f9aed
sharearahammed <sharearahammed@gmail.com>
2023-10-10 14:54:08 +0600

    commit

--- src/Doctor/doctor.module.ts
+++ src/Doctor/doctor.module.ts
@@ -2,12 +2,15 @@ import { Module, NestModule } from '@nestjs/common';
 import { DoctorController } from './doctor.controller';
 import { DoctorService } from './doctor.service';
 //import { DoctorMiddleware } from './doctor.middleware';
-import { MiddlewareBuilder } from '@nestjs/core';
+//import { MiddlewareBuilder } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Doctorinfo } from './doctorrinfo.dto';
 
 
 
 @Module({
   imports: [],
+  //imports: [TypeOrmModule.forFeature([Doctorinfo]),],
   controllers: [DoctorController],
   providers: [DoctorService],
 })
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,23 @@ import { Module } from '@nestjs/common';
 import { DoctorController } from './Doctor/doctor.controller';
 import { DoctorService } from './Doctor/doctor.service';
 import { ManagerController } from './manager.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DoctorModule } from './Doctor/doctor.module';
 
 @Module({
+  // imports: [DoctorModule,TypeOrmModule.forRoot(
+  //   { 
+  //   type: 'postgres',
+  //   host: 'localhost',
+  //   port: 5432,
+  //   username: 'postgres',
+  //   password: 'root',
+  //   database: 'abc_ecommerce',//Change to your database name
+  //   autoLoadEntities: true,
+  //   synchronize: true,
+  //   } ),],
   imports: [],
-  controllers: [DoctorController, ManagerController],
+  controllers: [DoctorController, ManagerController,],
   providers: [DoctorService],
 })
 export class AppModule {}
